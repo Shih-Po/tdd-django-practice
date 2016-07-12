b7b9b28b91666cc691a7c3c6d7582ae1e18b40d7
Shih-Po <spurcheng@gmail.com>
2016-07-12 19:30:59 +0800

    Add an URL for new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -6,54 +6,54 @@ from lists.models import Item
 
 from django.template.loader import render_to_string
 
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
+
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
 
-        response = home_page(request)
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
 
-        response = home_page(request)
+    def test_redirects_after_POST(self):
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
 
-    def test_home_page_only_saves_item_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
-
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
+
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
-
     def test_displays_all_items(self):
+
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
@@ -66,6 +66,7 @@ class ListViewTest(TestCase):
 class ItemModelTest(TestCase):
 
     def test_saving_and_retreaving_items(self):
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.save()
