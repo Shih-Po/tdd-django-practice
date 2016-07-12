ff999941dae1d67d7cc4067b62f0b2fa4200ace8
Shih-Po <spurcheng@gmail.com>
2016-07-12 16:59:30 +0800

    Add New test class for the unique list

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
-
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
